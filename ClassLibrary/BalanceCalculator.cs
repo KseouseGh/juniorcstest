3b2ae94eb15af1fdd4068c93b19414b0181947aa
Kseouse_Git <zakazakusok@gmail.com>
2025-02-23 12:41:25 +0300

    Balance of profile with calculator complete.

---
+++ ClassLibrary/BalanceCalculator.cs
@@ -0,0 +1,44 @@
+﻿using ConnectorTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class BalanceCalculator
+    {
+        private ITestConnector connector;
+
+        private BalanceCalculator(ITestConnector Connector)
+        {
+
+            connector = Connector;
+        }
+
+        public async Task<ProfileBalance> GetBalanceAsync()
+        {
+            var GeneralBalance = new ProfileBalance
+            {
+                BTC = 1m,
+                XRP = 15000m,
+                XMR = 50m,
+                DASH = 30m
+            };
+            var tickerBTCtoUSDT = (await connector.GetTickerAsync("BTCUSDT")).FirstOrDefault();
+            var tickerXRPtoUSDT = (await connector.GetTickerAsync("XRPUSDT")).FirstOrDefault();
+            var tickerXMRtoUSDT = (await connector.GetTickerAsync("XMRUSDT")).FirstOrDefault();
+            var tickerDASHtoUSDT = (await connector.GetTickerAsync("DASHUSDT")).FirstOrDefault();
+            var tickerBTCtoXRP = (await connector.GetTickerAsync("BTCXRP")).FirstOrDefault();
+            var tickerBTCtoXMR = (await connector.GetTickerAsync("BTCXMR")).FirstOrDefault();
+            var tickerBTCtoDASH = (await connector.GetTickerAsync("BTCDASH")).FirstOrDefault();
+            GeneralBalance.USDT = GeneralBalance.BTC * tickerBTCtoUSDT + GeneralBalance.XRP * tickerXRPtoUSDT + GeneralBalance.XMR * tickerXMRtoUSDT + GeneralBalance.DASH * tickerDASHtoUSDT;
+            GeneralBalance.BTC = GeneralBalance.BTC;
+            GeneralBalance.XRP = GeneralBalance.BTC * tickerBTCtoXRP;
+            GeneralBalance.XMR = GeneralBalance.BTC * tickerBTCtoXMR;
+            GeneralBalance.DASH = GeneralBalance.BTC * tickerBTCtoDASH;
+            return GeneralBalance;
+        }
+    }
+}
---
+++ ClassLibrary/ProfileBalance.cs
@@ -0,0 +1,18 @@
+﻿using ConnectorTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ProfileBalance
+    {
+        public decimal BTC { get; set; }
+        public decimal XRP { get; set; }
+        public decimal XMR { get; set; }
+        public decimal DASH { get; set; }
+        public decimal USDT { get; set; }
+    }
+}
